64bb687e064a1057ea20b3744b761934d2213ab1
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-09-15 20:13:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
+const picturesContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
-console.log(galleryItems);
+picturesContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+picturesContainer.addEventListener("click", onPicturesContainerClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+//
+function onPicturesContainerClick(event) {}
+
+// import * as basicLightbox from "basiclightbox";
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
